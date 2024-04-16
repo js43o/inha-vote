92fae6fc1c96e6345f0c33a68b3400b920ec29ad
Jiseung <js43o@naver.com>
2024-04-17 01:34:01 +0900

    💄 버튼 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,35 @@
+type ButtonProps = {
+  text: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  theme?: 'primary' | 'secondary';
+  icon?: React.ReactNode;
+  fullWidth?: boolean;
+  slim?: boolean;
+  small?: boolean;
+};
+
+export function Button({
+  text,
+  onClick,
+  theme = 'primary',
+  icon,
+  slim = false,
+  fullWidth = false,
+  small = false,
+}: ButtonProps) {
+  return (
+    <button
+      className={`flex cursor-pointer items-center justify-center gap-1 rounded-lg font-semibold leading-tight border
+      ${fullWidth ? 'w-full' : ''} ${small ? 'text-sm' : ''} ${slim ? 'px-2 py-1.5' : 'p-3'}
+      ${
+        theme === 'primary'
+          ? 'border-gray-800 bg-gray-800 text-white hover:bg-gray-700 active:bg-gray-900'
+          : 'border-gray-300 bg-gray-50 text-black hover:bg-white active:bg-gray-100'
+      }`}
+      onClick={onClick}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+}
