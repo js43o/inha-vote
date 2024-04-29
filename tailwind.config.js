b4b5067afcffb3cb5a651848672bd2f94d53dc08
js43o <js43o@naver.com>
2024-04-29 18:06:29 +0900

    🍱 로딩 원 아이콘 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                default: 'Pretendard',
-            },
-        },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        default: 'Pretendard',
+      },
+      animation: {
+        'spin-fast': 'spin 0.5s linear infinite',
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
