dea235ccd01a1de172206def1a88915fe7a1e3d4
Jiseung <js43o@naver.com>
2024-05-04 15:20:27 +0900

    ✨ useVoting 훅 추가 및 pre-vote 초안 작성

---
+++ src/libs/hooks/useVoting.ts
@@ -0,0 +1,44 @@
+import { randomHex } from 'web3-utils';
+import wc from '~/libs/circuit/witness_calculator.js';
+
+export function useVoting() {
+  const BN256ToBin = (str: string) => {
+    let r = BigInt(str).toString(2);
+    return `${[...Array(256 - r.length)].map((_) => '0').join('')}${r}`;
+  };
+
+  const preVote = async () => {
+    const secret = BigInt(randomHex(32)).toString();
+    const nullifier = BigInt(randomHex(32)).toString();
+
+    const input = {
+      secret: BN256ToBin(secret).split(''),
+      nullifier: BN256ToBin(nullifier).split(''),
+    };
+
+    const res = await fetch('/deposit.wasm');
+    const buffer = await res.arrayBuffer();
+    const depositWC = await wc(buffer);
+
+    const r = await depositWC.calculateWitness(input, 0);
+
+    const commitment = r[1];
+    const nullifierHash = r[2];
+
+    const tx = {
+      to: '', // Mixer 주소
+      value: BigInt(0), // 투표 토큰
+      /*
+      data: encodeFunctionData({
+        abi: '',
+        functionName: '',
+        args: [],
+      }),
+      */
+    };
+
+    // console.log(r);
+  };
+
+  return { preVote };
+}
